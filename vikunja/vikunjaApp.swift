4549847b4997f7af7263d8e3a42bd6047711c206
Sergio Suarez <sergios@backbase.com>
2026-08-24 18:53:13 -0600

    Initial Commit

---
+++ vikunja/vikunjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  vikunjaApp.swift
+//  vikunja
+//
+//  Created by Sergio Suárez álvarez on 24/08/26.
+//
+
+import SwiftUI
+
+@main
+struct vikunjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
