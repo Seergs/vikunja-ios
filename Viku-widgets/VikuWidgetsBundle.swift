27bbdeda76fd6dbfce626076af6d0d97c9e19bc2
Sergio Suarez <s.suarerez@gmail.com>
2026-09-03 19:34:02 -0600

    feat(widgets): add the large calendar month-grid widget

--- Viku-widgets/VikuWidgetsBundle.swift
+++ Viku-widgets/VikuWidgetsBundle.swift
@@ -14,6 +14,7 @@ import WidgetKit
 struct VikuWidgetsBundle: WidgetBundle {
     var body: some Widget {
         TodayWidget()
+        CalendarWidget()
         QuickAddWidget()
         GlyphWidget()
         if #available(iOS 18.0, *) {
